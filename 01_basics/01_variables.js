fd91ea1e5fa05eafae001e474a79987bdd97879b
Nip27 <146091492+Nip27@users.noreply.github.com>
2023-09-27 23:32:50 +0000

    Learning about variables and dataTypes

--- 01_basics/01_variables.js
+++ 01_basics/01_variables.js
@@ -1,8 +1,8 @@
-const accountIdd=144553
+const accountId=144553
 let accountEmail="nipamparmar72@gmail.com"
 var accountPassword="12345"
 accountCity="Jaipur"
 accountEmail="hc@hc.com"
 accountPassword="21212121"
 accountCity="Bengaluru"
-console.log([accountId,accountEmail,accountPassword,accountCity])
+console.table([accountId,accountEmail,accountPassword,accountCity])
---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; //treat all js code as newer version
+// alert(3+3) we are using node.js,not browser
+// code readability should be high
+let name="Nipam"
+let age="21"
+let isLoggedIn=true
+
+//number=>12345
+//bigint(for large number)
+//string=>"content"
+//boolean=>true/false
+
+//null(stand alone value)
+//undefined(value is not assigned till now)
+//typeof variable/value
